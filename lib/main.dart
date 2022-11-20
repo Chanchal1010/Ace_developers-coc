66abb3bf21ac75dd36e8ca8d6690349d39939044
Annu Kumari <annukumari1332004@gmail.com>
2022-11-20 16:32:43 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_1/pages/home_page.dart';
-
+import 'package:flutter_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,7 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+       // primaryTextTheme: GoogleFonts.latoTextTheme().
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/homepage": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
